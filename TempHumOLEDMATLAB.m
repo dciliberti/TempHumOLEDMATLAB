90a718adaf6f673ab5c6622dfa9ae72a1492d9cc
Danilo Ciliberti <52099779+dciliberti@users.noreply.github.com>
2019-08-15 11:29:38 +0200

    Added license boilerplate

--- TempHumOLEDMATLAB.m
+++ TempHumOLEDMATLAB.m
@@ -2,6 +2,18 @@
 % microcontroller. Acquire data from the sensors until a time limit is
 % reached. Plot last 10 minutes of live data during acquisition, the entire
 % data set after the acquisition, and save these data on a spreadsheet.
+%
+%    Copyright (C) 2018  Danilo Ciliberti dancili@gmail.com
+%    This program is free software: you can redistribute it and/or modify
+%    it under the terms of the GNU General Public License as published by
+%    the Free Software Foundation, either version 3 of the License, or
+%    (at your option) any later version.
+%    This program is distributed in the hope that it will be useful,
+%    but WITHOUT ANY WARRANTY; without even the implied warranty of
+%    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%    GNU General Public License for more details.
+%    You should have received a copy of the GNU General Public License
+%    along with this program.  If not, see <https://www.gnu.org/licenses/>
 
 close all
 instrreset
